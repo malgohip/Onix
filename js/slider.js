d846361827fb08847235f999e09c3492efe0113e
Jeaan <jninoba@unal.edu.co>
2024-08-14 21:49:12 +0000

    feat(structure): created the whole project

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    //!Automatic Slideshow ↓
+    //slideIndex++;
+    //if (slideIndex > slides.length) { slideIndex = 1 }
+    // setTimeout(showSlides, 3000); // Change image every 3 seconds
+    //!Automatic Slideshow ↑
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}

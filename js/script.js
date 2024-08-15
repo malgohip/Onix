152b14abf1990dad48ead7abcfbf8af52fc17918
Jeaan <jninoba@unal.edu.co>
2024-08-15 04:47:45 +0000

    feat(slider): added slider and fixed css issues

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let slideIndex = 1;
+let slideIndex = 1; 
+
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -15,6 +16,7 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
 
@@ -25,11 +27,7 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    //!Automatic Slideshow ↓
-    //slideIndex++;
-    //if (slideIndex > slides.length) { slideIndex = 1 }
-    // setTimeout(showSlides, 3000); // Change image every 3 seconds
-    //!Automatic Slideshow ↑
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }

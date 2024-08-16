57b4817d441f5032807bafd5d12c1826dbb6356d
Jeaan <jninoba@unal.edu.co>
2024-08-16 03:53:07 +0000

    feat(created login): created login and scripted slideshow (improvement pending)

--- js/script.js
+++ js/script.js
@@ -17,12 +17,22 @@ function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { 
+        slideIndex = 1 
+    }
+
+    if (n < 1) { 
+        slideIndex = slides.length 
+    }
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    } 
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
@@ -30,4 +40,5 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000)
 }

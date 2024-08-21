7f02377039a32851fd1993af7f7457cab4791a2f
Jeaan <jninoba@unal.edu.co>
2024-08-21 04:27:36 +0000

    fix: fixed the category container div

--- js/script.js
+++ js/script.js
@@ -26,19 +26,16 @@ function showSlides(n) {
     }
 
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none";  
     }
-
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    } 
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";  
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000)
+
+    // Uncomment the following line to automatically switch slides every 5 seconds:
+    setTimeout(() => plusSlides(1), 5000); 
 }
